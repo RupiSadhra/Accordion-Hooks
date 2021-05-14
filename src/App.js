8d248d8d5e9e7e2bd3afc7221b3f70cd7d7567d7
Rupi Sadhra <varinderjitkaur@developersinstitute.co.nz>
2021-05-14 22:37:33 +1200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Accordian from './components/Accordian'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const items=[
+    {
+        title:'What is React?',
+        content:'React is front end library to build reusable Ui components'
+    },
+    {
+        title:'Why use React?',
+        content:'React is favorite library among engineers'
+    },
+    {
+        title:'How do use React?',
+        content:'We use React by creating components'
+    }
+]
+const App=()=>{
+return <div><Accordian items={items}/></div>
 }
 
 export default App;
