817c6f3e0a18982c7db2a4ffb1faf0d342669b9c
Rupi Sadhra <varinderjitkaur@developersinstitute.co.nz>
2021-05-19 09:15:35 +1200

    pass state and setSelected to children

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
-import React from 'react';
-import Accordian from './components/Accordian';
-import Search from './components/Search';
+import React, { useState } from "react";
+import Dropdown from "./components/Dropdown";
+import Accordian from "./components/Accordian";
+import Search from "./components/Search";
 
-const items=[
-    {
-        title:'What is React?',
-        content:'React is front end library to build reusable Ui components'
-    },
-    {
-        title:'Why use React?',
-        content:'React is favorite library among engineers'
-    },
-    {
-        title:'How do use React?',
-        content:'We use React by creating components'
-    }
-]
-const App=()=>{
-return <div>
-    {/* <Accordian items={items}/> */}
-    <Search/>
-</div>
-}
+const items = [
+  {
+    title: "What is React?",
+    content: "React is front end library to build reusable Ui components",
+  },
+  {
+    title: "Why use React?",
+    content: "React is favorite library among engineers",
+  },
+  {
+    title: "How do use React?",
+    content: "We use React by creating components",
+  },
+];
+
+const options = [
+  {
+    label: "The Color Red",
+    value: "red",
+  },
+  {
+    label: "The Color Orange",
+    value: "orange",
+  },
+  {
+    label: "The Color Green",
+    value: "green",
+  },
+];
+
+const App = () => {
+  const [selected, setSelected] = useState(options[0]);
+
+  return (
+    <div>
+      {/* <Accordian items={items}/> */}
+      {/* <Search/> */}
+      <Dropdown
+        options={options}
+        selected={selected}
+        onSelectedChange={setSelected}
+      />
+    </div>
+  );
+};
 
 export default App;
